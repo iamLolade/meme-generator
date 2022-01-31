dd3eee9fa801bd857d2c0cbbe8ccb126fe569367
iamLolade <adeniyiotemade@gmail.com>
2022-02-01 00:33:50 +0100

    Created Navbar and Meme component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar";
+import Meme from "./components/Meme";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Meme />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return ( 
+        <nav className="nav">
+            <img 
+                src="./images/troll-face.png" 
+                className="nav--image"
+            />
+            <h2 className="nav--title">Meme Generator</h2>
+            <h4 className="nav--project">React Project</h4>
+        </nav>
+     );
+}
+ 
+export default Navbar;
